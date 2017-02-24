5330c7cbf58640471027b00a7ee72f2f7f580809
terrell linnell <terrell.linnell@gmail.com>
2017-02-24 13:39:54 -0700

    created route for getting and updating comments

--- routes/Post.js
+++ routes/Post.js
@@ -34,14 +34,19 @@ Router.route('/posts')
 
 Router.route('/posts/:id')
   .get(function (req, res) {
-    Post.findById(req.params.id, function (err, post) {
-      if (err) {
-        res.json({ message: 'there was an error finding this post' });
-      } else {
-        res.json(post);
-      }
-    });
-  })
+    Post.findById(req.params.id)
+      .populate({
+        path: 'comments',
+        populate: {path: author}
+      })
+      .exec(function (err, post) {
+        if (err) {
+          res.json({ message: 'there was an error finding this post' });
+        } else {
+          res.json(post);
+        }
+      })
+    })
   .put(function (req, res) {
     Post.findById(req.params.id, function (err, post) {
       if (err) {
@@ -106,34 +111,43 @@ Router.route('/posts/:id/comments')
   });
 });
 
-Router.route('/post/comments/:commentId')
+Router.route('/posts/comments/:commentId')
+.get(function (req, res) {
+  console.log('looking for a comment');
+  Comment.findById(req.params.commentId)
+    .populate('author')
+    .exec(function (err, comment) {
+      if (err) {
+        res.json({ message: 'there was an error finding this comment' });
+      } else {
+        res.json(comment);
+      }
+    });
+  })
 .put(function (req, res) {
-  Comment.findById(req.params.id, function (err, comment) {
-    if (comment.author === req.user._id) {
-      comment.text = req.body.text ? req.body.text : comment.text;
-      comment.save(function (err, data) {
-        if(err) {
-          res.json({error: err})
-        } else {
-          res.json({message: 'updated comment'})
-        }
-      });
+  Comment.findById(req.params.commentId, function (err, comment) {
+    if (err) {
+      console.log('Error finding comment');
+      res.json({'error' : err})
     } else {
-      res.json({message: 'you are not allowed to update this comment'})
+      if (req.user) {
+      if (comment.author.toString() === req.user._id.toString()) {
+        comment.text = req.body.text ? req.body.text : comment.text;
+        comment.save(function (err, data) {
+          if(err) {
+            res.json({error: err})
+          } else {
+            res.json({message: 'updated comment'})
+          }
+        });
+      } else {
+        res.json({message: 'you are not allowed to update this comment'})
+      }
+    } else {
+      res.json({error: 'you must be logged in to update comments'})
     }
-  });
+    }
+  })
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-  module.exports = Router;
+module.exports = Router;
