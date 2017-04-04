589da7fb18ba3908c4b22d713f7339f4804a43e2
terrell linnell <terrell.linnell@gmail.com>
2017-04-04 15:48:28 -0600

    post go from new to old from top to bottowm

--- routes/Post.js
+++ routes/Post.js
@@ -13,7 +13,7 @@ Router.route('/posts')
       path:     'comments',
       populate: {path: 'author'}
     })
-    .sort('date')
+    .sort('-date')
     .exec(function (err, posts) {
     if (err) {
       console.log(err);
